cf8d27ae394d198afc8b481b54a334d088eae4d8
zhangweiwei <12345678>
2020-10-31 12:43:09 +0800

    添加测试方法

--- src/main/java/com/demo/modules/bussiness/controller/IndexController.java
+++ src/main/java/com/demo/modules/bussiness/controller/IndexController.java
@@ -24,4 +24,67 @@ public class IndexController {
     public ResponeData<User> save(@RequestBody User user) {
         return new ResponeData(user);
     }
+
+    @GetMapping("test")
+    @ResponseBody
+    public String test() {
+        String jsonStr = "{\n" +
+                "\tpageNum: 1,\n" +
+                "\tpageSize: 50,\n" +
+                "\tsize: 0,\n" +
+                "\tstartRow: 0,\n" +
+                "\tendRow: 0,\n" +
+                "\ttotal: 0,\n" +
+                "\tpages: 0,\n" +
+                "\tprePage: 0,\n" +
+                "\tlist: [{\n" +
+                "\t\t\"lampPostId\": \"lp101814695421280256\",\n" +
+                "\t\t\"organId\": \"zm001\",\n" +
+                "\t\t\"lampPostTypeName\": \"双臂灯\",\n" +
+                "\t\t\"lampPostCode\": \"0123456433\",\n" +
+                "\t\t\"aloneLampCount\": 2,\n" +
+                "\t\t\"streetName\": \"经十路\",\n" +
+                "\t\t\"streetSideName\": \"北侧\",\n" +
+                "\t\t\"xTude\": 109.703982862289,\n" +
+                "\t\t\"yTude\": 35.503025216873,\n" +
+                "\t\t\"transformerName\": \"经十路控制箱\",\n" +
+                "\t\t\"abbreviate\": \"lp\",\n" +
+                "\t\t\"alonelamps\": [{\n" +
+                "\t\t\t\t\"aloneLampNumber\": 1,\n" +
+                "\t\t\t\t\"lampPower\": 200,\n" +
+                "\t\t\t\t\"lampTypeName\": \"LED灯\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"aloneLampNumber\": 2,\n" +
+                "\t\t\t\t\"lampPower\": 200,\n" +
+                "\t\t\t\t\"lampTypeName\": \"LED灯\"\n" +
+                "\t\t\t}\n" +
+                "\t\t]\n" +
+                "\t},{\n" +
+                "\t\t\"lampPostId\": \"lp101814695421280257\",\n" +
+                "\t\t\"organId\": \"zm001\",\n" +
+                "\t\t\"lampPostTypeName\": \"双臂灯\",\n" +
+                "\t\t\"lampPostCode\": \"0123456433\",\n" +
+                "\t\t\"aloneLampCount\": 2,\n" +
+                "\t\t\"streetName\": \"经十路\",\n" +
+                "\t\t\"streetSideName\": \"北侧\",\n" +
+                "\t\t\"xTude\": 109.703982862289,\n" +
+                "\t\t\"yTude\": 35.503025216873,\n" +
+                "\t\t\"transformerName\": \"经十路控制箱\",\n" +
+                "\t\t\"abbreviate\": \"lp\",\n" +
+                "\t\t\"alonelamps\": [{\n" +
+                "\t\t\t\t\"aloneLampNumber\": 1,\n" +
+                "\t\t\t\t\"lampPower\": 200,\n" +
+                "\t\t\t\t\"lampTypeName\": \"LED灯\"\n" +
+                "\t\t\t},\n" +
+                "\t\t\t{\n" +
+                "\t\t\t\t\"aloneLampNumber\": 2,\n" +
+                "\t\t\t\t\"lampPower\": 200,\n" +
+                "\t\t\t\t\"lampTypeName\": \"LED灯\"\n" +
+                "\t\t\t}\n" +
+                "\t\t]\n" +
+                "\t}]\n" +
+                "}";
+        return jsonStr;
+    }
 }
